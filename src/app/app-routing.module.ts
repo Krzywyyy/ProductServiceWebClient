dea7650ae89dfcd8f844f9e0b847141c9984c40a
Piotr <krzywusiakk@gmail.com>
2019-08-08 10:24:02 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductComponent } from './product/product.component';
+import { AddProductComponent } from './product/add-product/add-product.component';
+import { EfficientProductsComponent } from './product/efficient-products/efficient-products.component';
+import { BrokenProductsComponent } from './product/broken-products/broken-products.component';
+import { DeleteProductComponent } from './product/delete-product/delete-product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'products', component: ProductComponent},
+  {path: 'products/broken', component: BrokenProductsComponent},
+  {path: 'products/efficient', component: EfficientProductsComponent},
+  {path: 'products/add',component: AddProductComponent},
+  {path: 'products/delete',component: DeleteProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HttpService } from './service/http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from './register/register.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { ProductComponent } from './product/product.component';
+import { AddProductComponent } from './product/add-product/add-product.component';
+import { EfficientProductsComponent } from './product/efficient-products/efficient-products.component';
+import { BrokenProductsComponent } from './product/broken-products/broken-products.component';
+import { DeleteProductComponent } from './product/delete-product/delete-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TopNavbarComponent,
+    ProductComponent,
+    AddProductComponent,
+    EfficientProductsComponent,
+    BrokenProductsComponent,
+    DeleteProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
